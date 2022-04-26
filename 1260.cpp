c04a0c0746c6dcb8388ed30023764c8dd2137502
Dong-Ha <tlsehdgk33@gmail.com>
2022-04-26 20:59:57 +0900

    04-26 DFS BFS

---
+++ 1260.cpp
@@ -0,0 +1,70 @@
+// https://scarlettb.tistory.com/76의 코드
+// https://yunyoung1819.tistory.com/86 DFS BFS 설명
+#include <iostream>
+#include <queue>
+using namespace std;
+#define MAX 1001
+
+int N,M,V; //정점개수, 간선개수, 시작정점
+int map[MAX][MAX]; // 인접 행렬 그래프
+bool visited[MAX]; // 정점 방문 여부
+queue<int> q;
+
+void reset() {
+    for (int i=1; i<=N; i++){
+        visited[i]=0;
+    }
+}
+
+void DFS(int v){
+    visited[v] = true;
+    cout << v << " ";
+
+    for(int i =1; i<=N; i++){
+        if(map[v][i] == 1 && visited[i] == 0){ //현재 정점과 연결되어있고 방문되지 않았으면
+            DFS(i);
+        }
+    }
+
+}
+
+void BFS(int v){
+    q.push(v);
+    visited[v] = true;
+    cout << v << " ";
+
+    while(!q.empty()){
+        v = q.front();
+        q.pop();
+
+        for(int w = 1; w <= N; w++){
+            if(map[v][w] == 1 && visited[w] == 0){ //현재 정점과 연결되어있고 방문되지 않았으면
+                q.push(w);
+                visited[w] = true;
+                cout << w << " ";
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> N >> M >> V;
+
+    for(int i=0; i<M; i++){
+        int a,b;
+        cin >> a >> b;
+        map[a][b] = 1;  
+        map[b][a] = 1; // 반대도 연결되어있으므로
+    }
+
+    reset();
+    DFS(V);
+
+    cout << '\n';
+
+    reset();
+    BFS(V);
+
+
+    return 0;
+}
